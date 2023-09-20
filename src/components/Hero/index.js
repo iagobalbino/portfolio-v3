bebf52928ee5f1bf93df2f2e0e01783f3e40cb10
Iago Balbino <iago_nobre@hotmail.com>
2023-09-20 07:43:54 -0300

    Hero page estilizada

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -6,16 +6,16 @@ const Hero = (props) => {
       <h2 className='name-title'>
         {props.name}
       </h2>
-      <p>
+      <p className='job-title'>
         {props.jobTitle}
       </p>
       <div className='social-media'>
         <a href={props.urlLinkedin}>
-          <img src="images/linkedin.svg" alt="LinkedIn" />
+          <img src="images/linkedin_white.svg" alt="LinkedIn" />
         </a>
 
         <a href={props.github}>
-          <img src="images/github.svg" alt="Github" />
+          <img src="images/github_white.svg" alt="Github" />
         </a>
       </div>
     </section>
