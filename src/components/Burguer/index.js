ea14d285f8a9a98441f4fed1889f4ffe0efe7e2d
Iago Balbino <iago_nobre@hotmail.com>
2023-11-29 17:34:45 -0300

    Criado useState para o componente Burguer

--- src/components/Burguer/index.js
+++ src/components/Burguer/index.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
+import Menu from '../Menu';
 import './Burguer.css';
 
 const Burguer = () => {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <nav className='burguer-menu'>
+    <nav
+      className='burguer-menu'
+      onClick={() => setToggle(!false)}
+    >
       <div className='burguer-line' />
       <div className='burguer-line' />
       <div className='burguer-line' />
+      <Menu
+
+      />
     </nav>
   );
 };
