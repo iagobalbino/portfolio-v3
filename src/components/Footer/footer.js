d143d79f51009ac0e24e00678c46ab87549b1d37
Iago Balbino <iago_nobre@hotmail.com>
2024-03-03 09:51:59 -0300

    Footer criado

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,8 +1,17 @@
 import './Footer.css';
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
     <footer className='footer'>
+      <div className='footer-container'>
+        <p>Site desenhando e desenvolvido por Iago Balbino.</p>
+        <div className='icons'>
+          <FaLinkedin className='linkedin' />
+          <FaGithub className='github' />
+        </div>
+      </div>
 
     </footer>
   );
